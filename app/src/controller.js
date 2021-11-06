72dd479b1bec06181d9cbd9daa409f8c505044f1
mota1A4 <mfernandomotam@gmail.com>
2021-11-06 15:48:11 -0600

    [feat] permite añadir al stock y al log con código mas entendible usando promesas.

--- app/src/controller.js
+++ app/src/controller.js
@@ -1,21 +1,13 @@
-const {setAction} = require('./db/connection');
 const {
-    checkStock,
-    baseQuery,
+    queryCheckStock,
+    queryAddProduct,
+    queryReduceStock,
+    queryLogSale,
+    queryAddToRestockLog,
     queryProductExists,
-    reduceStock,
-    logSale,
-    productExists,
-    existencesOfCurrentMonth
+    queryAddToStock,
+    queryExistencesOfCurrentMonth
 } = require('./model');
-
-const getTime = callback => {
-    setAction(queryTime, callback);
-};
-
-const NOOP = (err, data) => {
-    // console.log({err, data})
-}
 
 const Controller = {
     addToStock({idProducto, cantidad, nombreProducto} = {}) {
@@ -26,9 +18,14 @@ const Controller = {
                     return;
                 }
 
-                cantidad *= cantidad;
+                cantidad *= 1;
 
-                const [resExists] = await productExists(idProducto);
+                if(cantidad > 30){
+                    reject(`Las compras por producto no deben superar las 30 unidades por mes.`);
+                    return;
+                }
+
+                const [resExists] = await queryProductExists(idProducto);
 
                 if (!resExists) {
                     reject('Ocurrió un error verificando la existencia de un producto');
@@ -36,67 +33,50 @@ const Controller = {
                 }
 
                 if (resExists.it_exists) {
-                    const [resExistences] = await existencesOfCurrentMonth(idProducto);
+                    const [resExistences] = await queryExistencesOfCurrentMonth(idProducto);
                     const existencesQty = resExistences.qty + cantidad;
 
-                    if(existencesQty <= 30) {
-                        // TODO: discount de stock y añadir log
+                    if(existencesQty > 30){
+                        reject(`Las compras por producto no deben superar las 30 unidades por mes. Cantidad: ${cantidad} | Stock: ${resExistences.qty}`);
+                        return;
                     }
 
-                    reject(`Las compras por producto no deben superar las 30 unidades por mes. Cantidad: ${cantidad} | Stock: ${resExistences.qty}`);
-                    return;
-                }
+                    const {affectedRows} = await queryAddToStock({idProducto, cantidad});
 
-                console.log({resExists: resExists.it_exists})
+                    if(affectedRows){
+                        const {affectedRows} = await queryAddToRestockLog({idProducto, cantidad});
+
+                        if(affectedRows){
+                            resolve('Procesado con éxito');
+                            return;
+                        }
+
+                        reject('Ocurrió un error al procesar el log');
+                        return;
+                    }
+
+                    reject('Ocurrió un error al añadir al stock');
+                    return;
+                } else {
+                    const {affectedRows} = await queryAddProduct({idProducto, nombreProducto, cantidad});
+
+                    if(affectedRows){
+                        const {affectedRows} = await queryAddToRestockLog({idProducto, cantidad});
+
+                        if(affectedRows){
+                            resolve('Procesado con éxito');
+                            return;
+                        }
+
+                        reject('Ocurrió un error al procesar el log');
+                        return;
+                    }
+
+                    reject('Ocurrió un error al crear el producto');
+                }
             } catch (err) {
                 reject(err.toString());
             }
-
-            reject('holi');
-            return;
-
-            const insertIntoLog = (err = null, resp = null) => {
-                if (err) {
-                    console.error(err);
-                    reject('Ocurrió un error al insertar el producto');
-                }
-
-                const QUERY = `INSERT INTO product_restock_log (product_id, qty)
-                               VALUES (${data.idProducto}, ${data.cantidad})`;
-                setAction(() => baseQuery(QUERY, NOOP), NOOP);
-
-                resolve('Procesado con éxito');
-                return true;
-            }
-
-            const insertAfterCheck = (err = null, resp = null) => {
-                if (err) {
-                    console.error(err);
-                    reject('Ocurrió un error despues de chequear si el producto existía');
-                    return;
-                }
-
-                const exists = resp[0][0].it_exists;
-                const stock = resp[1] && resp[1][0] && resp[1][0].qty ? resp[1][0].qty : 0;
-                const total = (data.cantidad * 1) + (stock * 1);
-
-                if (total <= 30) {
-                    if (!exists || Number(exists) === 0) {
-                        const QUERY = `INSERT INTO products (id, name, stock)
-                                       VALUES (${data.idProducto}, '${data.nombreProducto}', ${data.cantidad})`;
-                        setAction(() => baseQuery(QUERY, insertIntoLog));
-                    } else {
-                        const QUERY = `UPDATE products
-                                       set stock = stock + ${data.cantidad}
-                                       WHERE id = ${data.idProducto}`
-                        setAction(() => baseQuery(QUERY, insertIntoLog));
-                    }
-                } else {
-                    reject(`No se pueden ingresar más de 30 unidades por mes de un producto. unidades: ${stock}`);
-                }
-            }
-
-            setAction(() => queryProductExists(data.idProducto, insertAfterCheck), NOOP)
         })
     },
     processSale({idProducto, cantidad} = {}) {
@@ -114,7 +94,7 @@ const Controller = {
                     return;
                 }
 
-                const [resStock] = await checkStock(idProducto);
+                const [resStock] = await queryCheckStock(idProducto);
 
                 if (!resStock || typeof resStock === 'object' && !resStock.hasOwnProperty('stock')) {
                     reject(`No se encontró un producto con el id ${idProducto}`);
@@ -128,10 +108,10 @@ const Controller = {
                     return;
                 }
 
-                const {affectedRows} = await reduceStock({idProducto, cantidad});
+                const {affectedRows} = await queryReduceStock({idProducto, cantidad});
 
                 if (affectedRows) {
-                    const {insertId, affectedRows} = await logSale({idProducto, cantidad});
+                    const {insertId, affectedRows} = await queryLogSale({idProducto, cantidad});
 
                     if (insertId && affectedRows) {
                         resolve('Procesado correctamente');
--- app/src/model.js
+++ app/src/model.js
@@ -1,22 +1,38 @@
 const {connection} = require('./db/connection');
 
 const model = {
-    queryProductExists(id, callback) {
-        const QUERY = `SELECT EXISTS(SELECT 1 FROM products WHERE id = ${id}) as it_exists;
-        SELECT SUM(qty) as 'qty'
-        FROM product_restock_log
-        WHERE product_id = ${id} AND MONTH (created_at) = MONTH (CURRENT_DATE) AND YEAR (created_at) = YEAR (CURRENT_DATE)
-        group by(qty);`;
-        connection.query(QUERY, (err, rows) => {
-            if (err) {
-                console.error(err);
-                callback(err);
-            }
+    async queryAddToStock({idProducto, cantidad}){
+        return new Promise(((resolve, reject) => {
+            connection.query(`UPDATE products
+                              SET stock = stock + ${cantidad}
+                              WHERE id = '${idProducto}'`, (err, rows) => {
+                if(err) reject(err.toString());
 
-            callback(null, rows);
-        });
+                resolve(rows);
+            });
+        }))
     },
-    async productExists(idProducto) {
+    async queryAddToRestockLog({idProducto, cantidad}){
+        return new Promise(((resolve, reject) => {
+            connection.query(`INSERT INTO product_restock_log (product_id, qty)
+                               VALUES (${idProducto}, ${cantidad})`, (err, rows) => {
+                if(err) reject(err.toString());
+
+                resolve(rows);
+            })
+        }))
+    },
+    async queryAddProduct({idProducto, nombreProducto, cantidad}){
+        return new Promise((resolve, reject) => {
+            connection.query(`INSERT INTO products (id, name, stock)
+                              VALUES (${idProducto}, '${nombreProducto}', ${cantidad})`, (err, rows) => {
+                if(err) reject(err.toString());
+
+                resolve(rows);
+            });
+        })
+    },
+    async queryProductExists(idProducto) {
         return new Promise(((resolve, reject) => {
             connection.query(`SELECT EXISTS(SELECT 1 FROM products WHERE id = '${idProducto}') as it_exists;`, (err, rows) => {
                 if (err) reject(err.toString());
@@ -25,7 +41,7 @@ const model = {
             });
         }))
     },
-    async existencesOfCurrentMonth(idProducto) {
+    async queryExistencesOfCurrentMonth(idProducto) {
         return new Promise(((resolve, reject) => {
             connection.query(`SELECT SUM(qty) as 'qty'
                               FROM product_restock_log
@@ -38,17 +54,7 @@ const model = {
             })
         }))
     },
-    baseQuery(query, callback) {
-        connection.query(query, (err, rows) => {
-            if (err) {
-                console.error(err);
-                callback(err);
-            }
-
-            callback(null, rows);
-        });
-    },
-    async checkStock(productId) {
+    async queryCheckStock(productId) {
         return new Promise(((resolve, reject) => {
             connection.query(`SELECT stock
                               FROM products
@@ -59,7 +65,7 @@ const model = {
             });
         }));
     },
-    async reduceStock({idProducto, cantidad}) {
+    async queryReduceStock({idProducto, cantidad}) {
         return new Promise(((resolve, reject) => {
             connection.query(`UPDATE products
                               set stock = stock - ${cantidad}
@@ -70,7 +76,7 @@ const model = {
             });
         }))
     },
-    async logSale({idProducto, cantidad}) {
+    async queryLogSale({idProducto, cantidad}) {
         return new Promise(((resolve, reject) => {
             connection.query(`INSERT INTO product_sale_log (product_id, qty)
                               VALUES ('${idProducto};', '${cantidad}')`, (err, rows) => {
