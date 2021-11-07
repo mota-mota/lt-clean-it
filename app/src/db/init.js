55146688afc1b219e5ed1c7cd5b2e08bc54d6aaa
mota1A4 <mfernandomotam@gmail.com>
2021-11-06 21:05:52 -0600

    Cambio en el init.js para crear la base de datos para comando npm run db

--- app/src/db/init.js
+++ app/src/db/init.js
@@ -1,6 +1,9 @@
-const {connection, setAction} = require('./connection');
+const mysql = require('mysql');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const INIT_QUERY = `CREATE TABLE IF NOT EXISTS products (
+const INIT_QUERY = `CREATE DATABASE IF NOT EXISTS cleanit_db;
+                    CREATE TABLE IF NOT EXISTS cleanit_db.products (
                         id INT(6) AUTO_INCREMENT PRIMARY KEY,
                         name VARCHAR(255) NOT NULL,
                         stock INT(10) NOT NULL,
@@ -8,7 +11,7 @@ const INIT_QUERY = `CREATE TABLE IF NOT EXISTS products (
                         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
                     );
                     
-                    CREATE TABLE IF NOT EXISTS product_sale_log(
+                    CREATE TABLE IF NOT EXISTS cleanit_db.product_sale_log(
                         id INT(6) AUTO_INCREMENT PRIMARY KEY,
                         product_id int NOT NULL,
                         qty int NOT NULL,
@@ -17,7 +20,7 @@ const INIT_QUERY = `CREATE TABLE IF NOT EXISTS products (
                         FOREIGN KEY (product_id) REFERENCES products(id)
                     );
                     
-                    CREATE TABLE IF NOT EXISTS product_restock_log(
+                    CREATE TABLE IF NOT EXISTS cleanit_db.product_restock_log(
                         id INT(6) AUTO_INCREMENT PRIMARY KEY,
                         product_id int NOT NULL,
                         qty int NOT NULL,
@@ -26,15 +29,26 @@ const INIT_QUERY = `CREATE TABLE IF NOT EXISTS products (
                         FOREIGN KEY (product_id) REFERENCES products(id)
                     );`;
 
-const actionInit = callback => {
-    connection.query(INIT_QUERY, (err, rows) => {
-        if (err) {
-            console.error(err);
-            callback(err);
-        }
+const connection = mysql.createConnection({
+    host: process.env.CLEANIT_HOST,
+    port: process.env.CLEANIT_PORT,
+    user: process.env.CLEANIT_USER,
+    password: process.env.CLEANIT_PASSWORD,
+    multipleStatements: true
+});
 
-        callback(null, rows);
-    });
-}
+connection.query(INIT_QUERY, (err, rows) => {
+    if (err) {
+        console.error(err);
+    }
 
-setAction(actionInit, () => {})
+    console.log(rows);
+});
+
+connection.end(err => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    console.log('disconnected');
+});
